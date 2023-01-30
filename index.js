433302a283bc7f2d91a71dfe3d2e830f35bd6d48
MaghrabiaM <mahmoud.maghrabia@vodafone.com>
2023-01-30 13:45:12 +0200

    add

---
+++ index.js
@@ -0,0 +1,10 @@
+// require the libraries for actions
+// import core from '@actions/core';
+// import github from '@actions/github';
+
+// use an async function for the main tasks
+
+function main() {
+}
+
+main();
