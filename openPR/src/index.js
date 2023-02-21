0e2389efa12e9554999c67ae8d8916ad6f588043
MaghrabiaM <mahmoud.maghrabia@vodafone.com>
2023-02-21 15:08:30 +0200

    add list, labels, date

--- openPR/src/index.js
+++ openPR/src/index.js
@@ -7,63 +7,72 @@ const o_head = core.getInput("head");
 
 const { owner, repo } = github.context.repo;
 
-const o_base_branch = "main";
 
+async function listBranches() {
+  // list all branches in a repository
+  octokit.rest.repos.listBranches({
+    owner: owner,
+    repo: repo
+  }).then(({ data }) => {
+    // data contains an array of branch objects
+    console.log('data ' + data);
+    data.forEach(branch => {
+      console.log('current branch ' + branch.name);
+      // do PR for branches that starts with release
+      if (branch.name.startsWith('release'))
+        doAutoPR(branch.name);
+    });
 
-async function brnaches  (){
-  console.log('start ' + o_base_branch);
- // list all branches in a repository
-octokit.rest.repos.listBranches({
-  owner: owner,
-  repo: repo
-}).then(({ data }) => {
-  // data contains an array of branch objects
-  console.log('data ' + data);
-  data.forEach(branch => {
-    console.log('data PR  ' + branch);
-    if (branch.name.startsWith('test'))
-    doAutoPR(branch.name);
+  }).catch(error => {
+    console.log("error occured " + error);
+    console.error(error);
   });
-
-/*
-  console.log('filter ');
-  const testBranches = data.filter(e => e.startsWith('test'));
-  
-  testBranches.forEach(branch => {
-    console.log('testBranches ' + branch);
-    doAutoPR(branch);
-  });
-  */
-
-}).catch(error => {
-  console.error(error);
-});
 
 };
 
 
-async function doAutoPR(head_branch) {
-try {
-console.log(' owener ' + owner + ' repo ' + repo + '; ' + 'head_branch ' + head_branch  ); 
-const rest = octokit.rest.pulls.create({
-  owner, 
-  repo, 
-  base: 'main', 
-  head: head_branch, 
-  title:'Auto PR' 
-}); 
+async function doAutoPR(base_branch) {
+  try {
+    const date = new Date();
+    const formattedDate = date.toLocaleDateString("en-GB", {
+      day: "2-digit",
+      month: "2-digit",
+      year: "numeric"
+    });
+    console.log('owener ' + owner + ' repo ' + repo + '; ' + ' base_branch ' + base_branch);
+    const title = "chore: [Auto-PR] master to " + base_branch + ' ' + formattedDate;
+    octokit.rest.pulls.create({
+      owner,
+      repo,
+      base: base_branch,
+      head: 'master',
+      title: title
+    }).then(({ data }) => {
+      console.log("PR created ," + data.number + ' data ' + data);
+      // add a labels to a pull request
+      octokit.rest.issues.addLabels({
+        owner,
+        repo,
+        issue_number: data.number,
+        labels: ["auto-marge"]
+      }).then(({ data }) => {
+        console.log("label added" + data);
+      }).catch(error => {
+        console.error('error: ' + error);
+      });
+    }).catch(error => {
+      console.error('error: ' + error);
+    });
 
-} catch (error) {
-  core.setFailed('Error ' + error);
+  } catch (error) {
+    core.setFailed('Error ' + error);
+  }
 }
-}
 
-
-// run code 
 
 try {
-brnaches();
-console.log('done PR'); 
+  listBranches();
+  console.log('done PR');
 } catch (error) {
   core.setFailed('error e ' + error)
 }
